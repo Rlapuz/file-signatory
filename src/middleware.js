c8428863ea7ee8f20ef95fd2d31a3b935ada5f11
Raymund Lapuz <raymund.lapuz.x@bulsu.edu.ph>
2023-10-08 04:01:50 +0800

    fix middleware and protect routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,10 @@
 export { default } from "next-auth/middleware"
 
-// export const config = {
-//     matcher: ['/dashboard/:path*', '/admin']
-// }
-
-// deploy to vercel
 export const config = {
-    matcher: ['https://file-signatory.vercel.app/dashboard/:path*', 'https://file-signatory.vercel.app/admin']
+    matcher: ['/dashboard/:path*', '/admin']
 }
+
+// // deploy to vercel
+// export const config = {
+//     matcher: ['https://file-signatory.vercel.app/dashboard/:path*', 'https://file-signatory.vercel.app/admin']
+// }
