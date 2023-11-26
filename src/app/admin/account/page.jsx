6ca112f740d20cb0d63ab895f71c29bec00fa7a5
RAYMUND LAPUZ <raymund.lapuz.x@bulsu.edu.ph>
2023-11-26 18:43:00 +0800

    add toast-nottify and finalize the signatory

--- src/app/admin/account/page.jsx
+++ src/app/admin/account/page.jsx
@@ -2,7 +2,7 @@ import { FacultyMember } from "@/components/admin-components/local/FacultyMember
 import React from "react";
 
 export const metadata = {
-  title: "Meneses | Admin Account",
+  title: "Meneses | Admin Accounts",
 };
 
 export const AccountPage = () => {
